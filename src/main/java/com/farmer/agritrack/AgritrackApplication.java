e73debd43c3f505d551657256977dffb65a3dde8
Venkat <shivannagarivenkat1706@gmail.com>
2025-10-15 16:50:36 +0530

    Initial commit - Spring Boot setup

---
+++ src/main/java/com/farmer/agritrack/AgritrackApplication.java
@@ -0,0 +1,13 @@
+package com.farmer.agritrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgritrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgritrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmer/agritrack/AgritrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmer.agritrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgritrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
